5b12375a8f081121adeea0d2cba201b310037952
Tin Myo Win <tinmyowin@better.hr>
2023-06-22 00:02:07 +0630

    Birdboard build dashboard

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,15 +1,21 @@
 @extends ('layouts.app')
 
 @section('content')
-<div class="flex items-center">
+<div class="flex items-center mb-3">
     <h1 class="mr-auto">Birdboard</h1>
     <a href="/projects/create">New project</a>
 </div>
-<ul>
+<div class="flex">
     @forelse ($projects as $project)
-    <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
+        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
+            <h3 class="font-normal text-xl mb-6"> {{ $project->title }} </h3>
+            <div class="text-grey">
+                {{ str_limit($project->description, 100) }}
+            </div>
+        </div>
     @empty
-    <li>No projects yet.</li>
+        <div>No projects yet.</div>
     @endforelse
-</ul>
+</div>
+
 @endsection
