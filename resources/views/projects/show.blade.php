cbbb82c5f2cb820227327834be3cad0b6b30a36d
Tin Myo Win <tinmyowin356@gmail.com>
2023-06-22 14:05:39 +0630

    Frontend code for each project show

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,23 +3,34 @@
 @section('content')
 <header class="flex items-center mb-3 py-4">
         <div class="flex w-full items-end justify-between">
-                <h1 class="text-grey text-sm font-normal">My Projects</h1>
+                <p class="text-grey text-sm font-normal">
+                        <a href="/projects" class="text-grey text-sm font-normal no-underline">
+                                My Projects
+                        </a> / {{ $project->title }}
+                </p>
                 <a href="/projects/create" class="button">New project</a>
         </div>
 
 </header>
 <main>
-        <div>
-                <div>
-                        <h2 class="text-grey font-normal">Tasks</h2>
-                        <h2 class="text-grey font-normal">General Notes</h2>
-                </div>
-                <div>
-                        <div class="card">
-                                <h1>{{ $project->title }}</h1>
-                                <div> {{ $project->description }}</div>
-                                <a href="/projects">Go Back</a>
+        <div class="lg:flex -mx-3">
+                <div class="lg:w-3/4 px-3 mb-6">
+                        <div class="mb-8">
+                                <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                                <div class="card mb-3">Lorem Ipsum</div>
+                                <div class="card mb-3">Lorem Ipsum</div>
+                                <div class="card mb-3">Lorem Ipsum</div>
+                                <div class="card mb-3">Lorem Ipsum</div>
+                                <div class="card">Lorem Ipsum</div>
+
                         </div>
+                        <div>
+                                <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+                                <textarea class="card w-full" style="min-height: 200px;">Lorem Ipsum</textarea>
+                        </div>
+                </div>
+                <div class="lg:w-1/4 px-3">
+                        @include('projects.card')
                 </div>
         </div>
 </main>
