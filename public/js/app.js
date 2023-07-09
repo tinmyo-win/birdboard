5f19538964c2b76fb50060e6ea675db6441f97a2
Tin Myo Win <tinmyowin356@gmail.com>
2023-07-09 21:34:39 +0630

    added them switcher ui

--- public/js/app.js
+++ public/js/app.js
@@ -14570,7 +14570,7 @@ window.Vue = __webpack_require__(35);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', __webpack_require__(39));
+Vue.component('theme-switcher', __webpack_require__(39));
 
 var app = new Vue({
   el: '#app'
@@ -49186,7 +49186,7 @@ var Component = normalizeComponent(
   __vue_scopeId__,
   __vue_module_identifier__
 )
-Component.options.__file = "resources/js/components/ExampleComponent.vue"
+Component.options.__file = "resources/js/components/ThemeSwitcher.vue"
 
 /* hot reload */
 if (false) {(function () {
@@ -49195,9 +49195,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-299e239e", Component.options)
+    hotAPI.createRecord("data-v-4c03ee6f", Component.options)
   } else {
-    hotAPI.reload("data-v-299e239e", Component.options)
+    hotAPI.reload("data-v-4c03ee6f", Component.options)
   }
   module.hot.dispose(function (data) {
     disposed = true
@@ -49334,14 +49334,29 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-    mounted: function mounted() {
-        console.log('Component mounted.');
+    data: function data() {
+        return {
+            themes: {
+                'theme-light': '#f5f6f9',
+                'theme-dark': '#222'
+            },
+            selectedTheme: 'theme-light'
+        };
+    },
+    created: function created() {
+        // console.log(localStorage.getItem('theme'));
+        this.selectedTheme = localStorage.getItem('theme') || 'theme-light';
+    },
+
+
+    watch: {
+        selectedTheme: function selectedTheme() {
+            document.body.className = document.body.className.replace(/theme-\w+/, this.selectedTheme);
+
+            localStorage.setItem('theme', this.selectedTheme);
+        }
     }
 });
 
@@ -49353,38 +49368,33 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c(
+    "div",
+    { staticClass: "flex items-center mr-8" },
+    _vm._l(_vm.themes, function(color, theme) {
+      return _c("button", {
+        key: { theme: theme },
+        staticClass:
+          "rounded-full w-4 h-4 bg-default border mr-2 focus:outline-none",
+        class: { "border-accent": _vm.selectedTheme == theme },
+        style: { backgroundColor: color },
+        on: {
+          click: function($event) {
+            _vm.selectedTheme = theme
+          }
+        }
+      })
+    }),
+    0
+  )
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "container" }, [
-      _c("div", { staticClass: "row justify-content-center" }, [
-        _c("div", { staticClass: "col-md-8" }, [
-          _c("div", { staticClass: "card card-default" }, [
-            _c("div", { staticClass: "card-header" }, [
-              _vm._v("Example Component")
-            ]),
-            _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
-              _vm._v(
-                "\n                    I'm an example component.\n                "
-              )
-            ])
-          ])
-        ])
-      ])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 module.exports = { render: render, staticRenderFns: staticRenderFns }
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-    require("vue-hot-reload-api")      .rerender("data-v-299e239e", module.exports)
+    require("vue-hot-reload-api")      .rerender("data-v-4c03ee6f", module.exports)
   }
 }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
 </head>
 
-<body class="theme-dark text-default bg-page">
+<body class="theme-light text-default bg-page">
     <div id="app">
         <nav class=" bg-header">
             <div class="container mx-auto">
@@ -48,36 +48,23 @@
                     <div>
 
                         <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
+                        <div class="flex items-center ml-auto">
                             <!-- Authentication Links -->
                             @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             @endif
                             @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                <theme-switcher></theme-switcher>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            <a class="flex items-center text-default no-underline text-sm" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <img src="{{ avatar_url(auth()->user()->name) }}" alt="{{ auth()->user()->email }}'s avatar" class="rounded-full w-8 mr-2">
+                                Admin
+                            </a>
                             @endguest
-                        </ul>
+                        </div>
                     </div>
                 </div>
             </div>
